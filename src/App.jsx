856557c1f820c40c018fa37626fb539c3f2bbe66
SoinRoma <soinroma26@gmail.com>
2024-04-04 12:56:15 +0500

    - updated plugin and added function for save

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import { Excalidraw } from "@excalidraw/excalidraw"
+import {Excalidraw, MainMenu, exportToBlob } from "@excalidraw/excalidraw"
+import {useState} from "react"
 
 function App() {
+  const [excalidrawAPI, setExcalidrawAPI] = useState(null)
+
+  async function saveSticker() {
+    if (excalidrawAPI) {
+      const elements = excalidrawAPI.getSceneElements()
+      if (elements || elements.length) {
+        const blob = await exportToBlob({
+          elements,
+          appState: {
+            exportWithDarkMode: false,
+            exportBackground: false,
+          },
+          files: excalidrawAPI.getFiles(),
+          getDimensions: (width, height) => { return {width: width, height: height}},
+          exportPadding: 5,
+        })
+        const file = new File([blob], "name.png", {type: 'image/png'})
+        const formData = new FormData()
+        formData.append('file', file)
+        await fetch('https://3a20-84-54-115-9.ngrok-free.app/sticker/create/', {
+          method: 'POST',
+          body: formData,
+          mode: 'no-cors',
+        })
+      }
+    }
+  }
+
   return (
     <>
-      <h1 style={{ textAlign: "center" }}>Excalidraw Example</h1>
-      <div style={{ height: "500px" }}>
-        <Excalidraw />
+      <div style={{height: "97vh"}}>
+        <Excalidraw langCode="ru-RU" excalidrawAPI={(api)=> setExcalidrawAPI(api)}>
+          <MainMenu>
+            <MainMenu.Item onSelect={() => saveSticker()}>Сохранить стикер</MainMenu.Item>
+            <MainMenu.DefaultItems.ClearCanvas/>
+            <MainMenu.DefaultItems.ChangeCanvasBackground/>
+          </MainMenu>
+        </Excalidraw>
       </div>
     </>
   );
