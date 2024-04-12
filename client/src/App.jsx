998ffc96daefce47100e5321ce192c40e1319d2a
Soin Roman <soinroma26@gmail.com>
2024-04-12 15:43:14 +0500

    - added logs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,9 +6,12 @@ function App() {
   const [excalidrawAPI, setExcalidrawAPI] = useState(null)
 
   async function saveSticker() {
+    console.log('Заходим в функцию!')
     if (excalidrawAPI) {
+      console.log('Есть excalidrawAPI')
       const elements = excalidrawAPI.getSceneElements()
       if (elements || elements.length) {
+        console.log('Есть elements')
         const blob = await exportToBlob({
           elements,
           appState: {
@@ -28,14 +31,18 @@ function App() {
         const formData = new FormData()
         formData.append('file', file)
         formData.append('initData', initData)
+        console.log('До отправки!')
         const response = await fetch('https://stickerpainterbot.altek.uz/sticker/create/', {
           method: 'POST',
           body: formData,
         })
         if (response.ok) {
           try {
+            console.log('После отправки')
             window.Telegram.WebApp.close()
           } catch (e) {
+            console.log('Ошибка')
+            console.log(e)
           }
         }
       }
@@ -46,47 +53,16 @@ function App() {
     const tg = window.Telegram.WebApp //получаем объект webapp телеграма
 
     tg.expand() //расширяем на все окно
-    tg.MainButton.text = "Save" //изменяем текст кнопки
-    tg.MainButton.textColor = "#FFFFFF"//изменяем цвет текста кнопки
+
+    tg.MainButton.text = "Save";//изменяем текст кнопки
+    tg.MainButton.textColor = "#FFFFFF" //изменяем цвет текста кнопки
     tg.MainButton.color = "#70b1ec" //изменяем цвет бэкграунда кнопки
     tg.MainButton.show()
 
-    window.Telegram.WebApp.onEvent('mainButtonClicked', async function(){
+    window.Telegram.WebApp.onEvent('mainButtonClicked', function(){
       tg.MainButton.showProgress(true)
-      if (excalidrawAPI) {
-        const elements = excalidrawAPI.getSceneElements()
-        if (elements || elements.length) {
-          const blob = await exportToBlob({
-            elements,
-            appState: {
-              exportWithDarkMode: false,
-              exportBackground: false,
-            },
-            files: excalidrawAPI.getFiles(),
-            getDimensions: (width, height) => {
-              const max = width > height ? width : height
-              const scale = 512 / max
-              return {width: width > height ? 512 : width * scale, height: height > width ? 512 : height * scale, scale}
-            },
-            exportPadding: 10,
-          })
-          const file = new File([blob], "name.png", {type: 'image/png'})
-          const initData = window.Telegram.WebApp.initData
-          const formData = new FormData()
-          formData.append('file', file)
-          formData.append('initData', initData)
-          const response = await fetch('https://stickerpainterbot.altek.uz/sticker/create/', {
-            method: 'POST',
-            body: formData,
-          })
-          if (response.ok) {
-            try {
-              window.Telegram.WebApp.close()
-            } catch (e) {
-            }
-          }
-        }
-      }
+      console.log('Нажимае на кнопку!')
+      saveSticker()
     })
   }, [])
 
